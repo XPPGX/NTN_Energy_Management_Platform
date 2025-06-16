ac5c1c4b9e3487e7519f056f3d9ba8afb2035f88
Arthur <qwerlegendary@gmail.com>
2025-06-16 13:47:22 +0800

    [Add] 新增卡片 [Add]導入卡片唯一GUID機制

--- demoVer/wwwroot/js/common.js
+++ demoVer/wwwroot/js/common.js
@@ -1,3 +1,11 @@
 window.checkIsMobile = function () {
     return window.matchMedia("(max-width: 1200px)").matches;
 }
+
+window.scrollToBottom = function () {
+    console.log("scroll to bottom !!!");
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+}
