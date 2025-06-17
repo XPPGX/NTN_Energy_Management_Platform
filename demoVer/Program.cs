f5ceb085b0f3d74ec4021effb6eeff7ebaca1716
Arthur <Arthur@meanwell.com>
2025-06-17 18:34:51 +0800

    [Sync]同步demo_ver1改動 [Add] 手機按壓一秒才能拖動 [Fix] framework修改成MW logo [Fix] 配合機器IP動態跳轉至框架

--- demoVer/Program.cs
+++ demoVer/Program.cs
@@ -1,7 +1,8 @@
 using demoVer.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:5043");
+builder.WebHost.UseUrls("http://127.0.0.1:5070");
+// builder.WebHost.UseUrls("http://0.0.0.0:5043");
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
