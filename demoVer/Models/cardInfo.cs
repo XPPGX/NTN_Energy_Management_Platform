9f61ceb8d168c4d3e20b0005ddc602518c2e4955
Arthur <Arthur@meanwell.com>
2025-06-20 19:08:18 +0800

    [Add]可編輯卡片後顯示於畫面上
    [Pending] 首頁讀取卡片設定之json檔案創建與儲存

--- demoVer/Models/cardInfo.cs
+++ demoVer/Models/cardInfo.cs
@@ -13,13 +13,8 @@ namespace demoVer.Models
         public CardType card_Type {get; set;}                                  
         public string widthClass {get; set;}                                //定義的寬度(w25 | w50)
         public byte showSequence {get; set;}                                //存被渲染的順序 start from 0
-        
-        // public Type ComponentType {get; set;}                               //razor的元件類型(名稱)
-        // public bool IsVisible {get; set;}                                   //控制可不可視
-        
-
-        //public LayoutComponentBase ComponentRef {get; set;}                 //存Ref，之後可用這個在C#控制這些child razor元件
-        //public Dictionary<string, object> Parameters {get; set;} = new();   //傳入每個card的參數
+        public string cardName {get; set;}                                  //卡片名稱
+        public List<INFO_DataItem> Info_DataItems {get; set;}               //存多個INFO_DataItems
     }
 
     public class InfoTuple
@@ -27,5 +22,61 @@ namespace demoVer.Models
         public string IconPath { get; set; }
         public string AltText { get; set; }
         public List<(string Label, string Value)> DataPairs { get; set; }
+    }
+
+    public enum INFO_SETTING_STAGE
+    {
+        CLOSE_ALL                       = 0,
+        ADD_ROW_STAGE                   = 1,
+        SELECT_ICON_STAGE               = 2,
+        SETTING_VALUE_AND_CMD_STAGE     = 3,
+        MACHINE_SELECTION_STAGE         = 4,
+        CMD_SELECTION_STAGE             = 5,
+        UNIT_SELECTION_STAGE            = 6,
+        ADD_VALUE_AND_CMD_ROW_STAGE     = 7,
+    }
+    
+    public enum SettingStatus
+    {
+        YET         = 1,
+        PROGRESS    = 2,
+        DONE        = 3,
+    }
+
+    public class INFO_DataFormat
+    {
+        public string MachineName {get; set;}
+        public string Cmd {get; set;}
+        public string ValueName {get; set;}
+        public string Unit  {get; set;} 
+
+        public INFO_DataFormat DeepClone()
+        {
+            return new INFO_DataFormat
+            {
+                MachineName = this.MachineName,
+                Cmd = this.Cmd,
+                ValueName = this.ValueName,
+                Unit = this.Unit
+            };
+        }
+    }
+
+    public class INFO_DataItem
+    {
+        public string iconPath { get; set; }
+        public SettingStatus status {get; set; } = SettingStatus.YET;
+        
+        public List<INFO_DataFormat> INFO_DataFormats {get; set;} = new();
+
+        public INFO_DataItem DeepClone()
+        {
+            return new INFO_DataItem
+            {
+                iconPath = this.iconPath,
+                status = this.status,
+                INFO_DataFormats = this.INFO_DataFormats.Select(data_format => data_format.DeepClone()).ToList()
+            };
+        }
     }
 }
