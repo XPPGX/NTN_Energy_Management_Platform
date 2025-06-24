364333e987aee50d4c339daed2ccebc68d0c363f
Arthur <Arthur@meanwell.com>
2025-06-24 15:16:39 +0800

    [Fix] 阻擋google自動翻譯 [Add] 可對InfoDataFormat電腦右鍵、手機長按 以編輯或刪除 [Pend] 對InfoDataItem電腦右鍵、手機長按 以編輯或刪除 [Pend] 選擇設備、選擇命令的List嘗試用MudBlazor做

--- demoVer/wwwroot/js/common.js
+++ demoVer/wwwroot/js/common.js
@@ -14,3 +14,100 @@ function jumpToApp(port) {
     const host = window.location.hostname;
     window.location.href = `http://${host}:${port}/`;
 }
+
+
+// window.registerGroupedTableTouchAndRightClick = function (containerId, dotNetHelper, pressTime = 800) {
+//     const container = document.getElementById(containerId);
+//     if (!container) return;
+
+//     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+//     let timer = null;
+
+//     function getElementInfo(el) {
+//         const table = el.getAttribute("data-table");
+//         const index = parseInt(el.getAttribute("data-index"));
+//         const rect = el.getBoundingClientRect();
+//         return { table, index, x: rect.right, y: rect.top + rect.height };
+//     }
+
+//     container.addEventListener("touchstart", function (e) {
+//         if (!isMobile) return;
+//         const el = e.target.closest(".long-press-row");
+//         if (!el) return;
+//         timer = setTimeout(() => {
+//             const info = getElementInfo(el);
+//             dotNetHelper.invokeMethodAsync("OnShowContextMenu", info.table, info.index, info.x, info.y);
+//         }, pressTime);
+//     });
+
+//     container.addEventListener("touchend", () => clearTimeout(timer));
+//     container.addEventListener("touchmove", () => clearTimeout(timer));
+
+//     container.addEventListener("contextmenu", function (e) {
+//         if (isMobile) return;
+//         const el = e.target.closest(".long-press-row");
+//         if (!el) return;
+//         e.preventDefault();
+//         const info = getElementInfo(el);
+//         dotNetHelper.invokeMethodAsync("OnShowContextMenu", info.table, info.index, info.x, info.y);
+//     });
+// };
+
+let longPressTriggered = false;
+window.registerGroupedTableTouchAndRightClick = function (containerId, dotNetHelper, pressTime = 400) {
+    const container = document.getElementById(containerId);
+    if (!container) return;
+
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    let timer = null;
+
+    function getElementInfo(el) {
+        const table = el.getAttribute("data-table");
+        const index = parseInt(el.getAttribute("data-index"));
+        // const rect = el.getBoundingClientRect();
+        return { table, index};
+    }
+
+    container.addEventListener("touchstart", function (e) {
+        if (!isMobile) return;
+        const el = e.target.closest(".long-press-row");
+        const touch = e.touches[0];
+        if (!el) return;
+        longPressTriggered = false;
+        timer = setTimeout(() => {
+            longPressTriggered = true;
+            const info = getElementInfo(el);
+            const x = touch.clientX;
+            const y = touch.clientY;
+            dotNetHelper.invokeMethodAsync("OnShowContextMenu", info.table, info.index, x, y);
+        }, pressTime);
+        console.log("[touchstart]", e);
+        dotNetHelper.invokeMethodAsync("JS_Console", "[touchstart]" + e);
+    });
+
+    container.addEventListener("touchend", function(e)
+    {
+        if(!longPressTriggered){
+            clearTimeout(timer);
+        }
+        console.log("[touchend]", e);
+        dotNetHelper.invokeMethodAsync("JS_Console", "[touchend]" + e);
+    });
+    container.addEventListener("touchmove", function(e)
+    {
+        clearTimeout(timer);
+        console.log("[touchmove]", e);
+        dotNetHelper.invokeMethodAsync("JS_Console", "[touchmove]" + e);
+    });
+
+    container.addEventListener("contextmenu", function (e) {
+        if (isMobile) return;
+        const el = e.target.closest(".long-press-row");
+        if (!el) return;
+        e.preventDefault();
+        const info = getElementInfo(el);
+        const x = e.clientX
+        const y = e.clientY
+        dotNetHelper.invokeMethodAsync("OnShowContextMenu", info.table, info.index, x, y);
+    });
+};
